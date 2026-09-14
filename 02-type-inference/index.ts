d9ade233cc621335e0dbafc1bccf4cb16e7c2c04
abdul-moiz1687 <abdulmoizarain766@gmail.com>
2026-09-14 16:19:26 +0500

    Add type inference practice

---
+++ 02-type-inference/index.ts
@@ -0,0 +1,36 @@
+//Type Inference kya hoti hai?
+//TypeScript khud variable ki type guess kar leta hai.
+
+let userName = "Moiz";
+let age = 18;
+let isStudent = true;
+
+let fruit =["Mango","Banana","Apple"]
+fruit.push("Graphes")
+
+let user={
+    name:"Moiz",
+    age:18,
+    isStudent:true
+}
+user.age=19
+
+//let + later assignment
+//let message;
+//let message = "Hello";
+// message = "Hi"; // ✅
+// message = 123;  // ❌
+//Kyuki initial value "Hello" se TypeScript ne message ko string infer kar liya.
+
+
+//const ka thoda different behavior
+//const name = "Moiz";
+
+
+let city="Karachi"
+city="Lahore"
+
+let mark = 20
+mark=90
+
+const country = "Pakistan"
